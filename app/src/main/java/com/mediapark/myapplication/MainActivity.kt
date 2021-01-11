2000d2bf6377612b4e21e6fa2a1022d4b54c06ab
denis zhuravlev <denis.zhuravlev.it@gmail.com>
2021-01-11 20:38:20 +0200

    Initial commit

---
+++ app/src/main/java/com/mediapark/myapplication/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.mediapark.myapplication
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        var name = "fdfdfd"
+        setContentView(R.layout.activity_main)
+        name = "aaaaaa"
+
+        // ctrl + shift + n
+        // ctrl + e
+        // ctrl + alt + left/right
+        // alt + f7
+        // ctrl + alt + O + I + L
+        // ctrl + f \r
+        // ctrl + i
+        // ctrl + d
+    }
+}
